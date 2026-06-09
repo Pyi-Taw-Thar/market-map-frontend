fa0fa0f25c23ca58e2c4aaffb9437850e88f64f6
acehidan <aungaungoogame2@gmail.com>
2026-06-09 18:49:02 +0630

    Rename app to PTT Marketing Map

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
                   </svg>
                 </div>
                 <h1 className="text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-indigo-600 to-violet-600">
-                  Field Ops
+                  PTT Marketing Map
                 </h1>
               </div>
 
